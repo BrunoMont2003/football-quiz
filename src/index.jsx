8855d336d59d086d6a92b54abb0837c720411728
brunomont <brunoadrian.mont@gmail.com>
2022-05-14 10:17:03 -0500

    add: scss

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import { App } from './App'
-import './styles/globals.css'
+import './styles/globals.scss'
 
 const root = document.getElementById('root')
 const container = createRoot(root)
